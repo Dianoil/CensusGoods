97ef2bd6601db9a26eb97e24f1baac007f36caba
Диана Абдулкадирова <a539user132@gmail.com>
2022-03-24 15:41:03 +0300

    com

---
+++ CensusGoods/Wind/ChangeInfo.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CensusGoods.Wind
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeInfo.xaml
+    /// </summary>
+    public partial class ChangeInfo : Window
+    {
+        public ChangeInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void saveinfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
