7c9892d0846acdcf0efcbf546b531c1be2543b31
Диана Абдулкадирова <a539user132@gmail.com>
2022-04-21 13:45:48 +0300

    com14Page

---
+++ CensusGoods/View/AddOrder.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CensusGoods.Wind
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrder.xaml
+    /// </summary>
+    public partial class AddOrder : Window
+    {
+        public AddOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void GosNumSuppl_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GosNumSuppl_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GosNumOrder_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GosNumOrder_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportProd_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportProd_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CargWeight_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CargWeight_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DateStart_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DateStart_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CbNameSector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CbSaleSector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
