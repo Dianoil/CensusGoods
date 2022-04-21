75218c50fd6a9c7330b2eb959b53266b451f97e5
Диана Абдулкадирова <a539user132@gmail.com>
2022-04-21 13:45:48 +0300

    com14Page

---
+++ CensusGoods/View/AddCompany.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CensusGoods.Wind
+{
+    /// <summary>
+    /// Логика взаимодействия для AddCompany.xaml
+    /// </summary>
+    public partial class AddCompany : Window
+    {
+        public AddCompany()
+        {
+            InitializeComponent();
+        }
+
+        private void City_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void City_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextStage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NameCompany_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NameCompany_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void INN_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void INN_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ogrn_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ogrn_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CensusGoods/View/AddSupply.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CensusGoods.Wind
+{
+    /// <summary>
+    /// Логика взаимодействия для AddSupply.xaml
+    /// </summary>
+    public partial class AddSupply : Window
+    {
+        public AddSupply()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportProd_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImportProd_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
